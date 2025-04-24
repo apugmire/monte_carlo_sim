f3f6184a9ff303d1e3473de185a7fcc8c6df0c7b
Alysa Pugmire <alysapugmire@gmail.com>
2025-04-24 00:05:10 -0400

    Update die file

--- monte_carlo/die.py
+++ monte_carlo/die.py
@@ -28,7 +28,9 @@ class Die():
         self._die.index.name = "Faces"
         
     def change_weight(self,face,weight):
-        '''changes the weight of one face if that face is on the die and the weight is a valid value'''
+        '''changes the weight of one face if that face is on the die and the weight is a valid value (must be an integer
+        or float).
+        '''
         
         #check if provided value is a face
         if face not in list(self._die.index):
@@ -45,7 +47,7 @@ class Die():
         self._die.loc[(face,'weights')] = weight
     
     def roll_die(self, rolls=1):
-        '''roll the die a specificed amount of times. Default is 1 roll'''
+        '''takes an integer parameter to specify how many times the dice should be rolled. Default is 1 roll'''
         
         #calculate probablity of rolling each face
         prob = [i/sum(self._die.weights) for i in self._die.weights]
@@ -89,8 +91,9 @@ class Game():
         
         # convert dictionary into a dataframe where the column names are the list index 
         # and the row names are the roll number
-        self._play_results  = pd.DataFrame(outcome, index=[i+1 for i in range(0,rolls)])
+        self._play_results  = pd.DataFrame(outcome, index=[i for i in range(0,rolls)])
         self._play_results.index.name = "roll_num"
+        self._play_results.columns.name = "dice_num"
         
     def show_outcome(self, form="wide"):
         '''
@@ -110,10 +113,81 @@ class Game():
         
         # if user indicates wide form, format data frame and return a copy
         elif self.form == "narrow":
-            narrow = self._play_results.copy()
-            narrow
-            ## convert play_game dataframe to narrow format and return that
+            
+            # convert copy of wide data frame into narrow version
+            narrow = self._play_results.copy().stack()
+        
+            #set multiindex names
+            narrow.index.set_names(["roll_num","die_num"], inplace=True)
+            
+            # turn into a dataframe
+            narrow = narrow.to_frame(name="outcome")
+            
+            # return narrow format data frame
+            return narrow
         
         # if the uswer enters anything else, raise a error
         else:
             raise ValueError("Please specify either narrow or wide.")
+
+class Analyzer():
+    '''
+    This class provides methods for analyzing the results of a single game. These methods require a game from the Game 
+    class. This class takes the results of a single game and computes various descriptive statistical properties about it.
+
+    Attributes:
+    
+    game: an instantiated game object (type: Game)
+    '''
+    
+    def __init__(self,game):
+        '''
+        Takes a game object as its input parameter. Throws a ValueError if the passed value is not a Game object.
+        '''
+        if type(game) != Game:
+            raise ValueError("This method only accepts Game objects")
+        else:
+            self.game = game
+    
+    def jackpot(self):
+        '''
+        Takes no parameters. Computes how many times the game resulted in a jackpot. A jackpot is a result in which all
+        faces are the same. Returns an integer for the number of jackpots.
+        '''
+        # get a series which shows the number of unique values in each row
+        uniques = self.game._play_results.nunique(axis=1)
+        
+        # find home many rows in this series have only 1 unique value
+        jackpot = sum(uniques== 1)
+        
+        # return number of jackpots
+        return jackpot
+             
+    def face_count(self):
+        '''
+        Takes no parameters. Computes how many times each face is rolled in each event. Returns a dataframe where the 
+        index is the roll number, the columns are the face values, and cells are the count values.
+        '''
+        # get list of all possible faces to use a column
+        all_faces = np.unique(np.concatenate([self.die.faces for piece in self.game.pieces]))
+        
+        #create base dataframe with roll number as index and possible faces as columns
+        counts = pd.DataFrame(index=self.game._play_results.index, columns = all_faces).fillna(0)
+        
+        #irterate over rolls and get counts of each face
+        for roll in self.game._play_results.index:
+        
+            # isolate each row
+            outcomes = self.game._playe_results.loc[roll].values
+            
+            # count occurance of each face and store in a dictionary where the face is the key and the count is the value
+            counts = {face:(outcomes==face).sum() for face in all_faces}
+            
+            #add counts to data frame
+            for face, count in count.items():
+                count.loc[roll_num,face] = count
+        return counts
+    
+    def combo_count(self):
+    
+    def perm_count(self):
