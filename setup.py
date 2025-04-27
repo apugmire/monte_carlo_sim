95a4bb1cf8df1ed05f37d15b207f6d066d0fb619
Alysa Pugmire <alysapugmire@gmail.com>
2025-04-27 16:12:10 -0400

    Update monte.py module

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='monte_carlo',
+    version='0.1',
+    description='A package that implements a simple monte carlo simulator',
+    url='https://github.com/apugmire/monte_carlo_sim',
+    author='A. Pugmire',
+    author_email='amp3xs@virginia.edu',
+    license='MIT',
+    packages=find_packages()
+)
